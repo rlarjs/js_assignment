c87f98c2c3d74b38f885ddf891420fc05d10cfb1
rlarjs <56258754+rlarjs@users.noreply.github.com>
2020-05-29 22:01:57 +0900

    Update Input_name.js

--- js_assigntment/js/Input_name.js
+++ js_assigntment/js/Input_name.js
@@ -4,7 +4,7 @@ person = prompt("input your name:");
 if(person == null || person =="")
   txt ="User cancelled the prompt.";
 else {
-  txt = person;
+  txt = "My name is " + person;
 }
-document.getElementById("demo").innerHTML = "My name is " + txt;
+document.getElementById("demo").innerHTML =  txt;
 }
